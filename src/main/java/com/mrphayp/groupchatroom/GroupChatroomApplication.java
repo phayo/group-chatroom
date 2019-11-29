be6fd499f9b741b0a4866f1376c40897e2b0ab20
phayo <anazodogodwin@gmail.com>
2019-11-29 20:29:05 +0100

    initial commit

---
+++ src/main/java/com/mrphayp/groupchatroom/GroupChatroomApplication.java
@@ -0,0 +1,13 @@
+package com.mrphayp.groupchatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupChatroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrphayp/groupchatroom/GroupChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrphayp.groupchatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
